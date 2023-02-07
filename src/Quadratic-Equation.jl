2a3b4268a455c0e44791133ba6f7a36eaa2531c0
Prince Okere <okereprince360@gmail.com>
2023-02-07 14:12:27 +0100

    Changes made

--- src/Quadratic-Equation.jl
+++ src/Quadratic-Equation.jl
@@ -7,7 +7,7 @@ function QuadEqua(a, b, c)
         rs = sqrt(D)/(2*a); realside = round(rs, digits = 3)
 
         paf1, naf1 = (root + realside), (root - realside)
-        println("x₁ = $(paf1) and x₂ = $(naf1)")
+        println("\nx₁ = $(paf1)\n" * "x₂ = $(naf1)")
 
     elseif (D == 0)
         paf_naf = root
@@ -17,6 +17,27 @@ function QuadEqua(a, b, c)
         imS = sqrt(D * -1)im/(2*a); imagside = round(imS, digits = 3)
 
         paf2, naf2 = (root + imagside), (root - imagside)
-        println("x₁ = $(paf2) and x₂ = $(naf2)")
+        println("\nx₁ = $(paf2)\n" * "x₂ = $(naf2)")
+    end
+end;
+
+while true
+    try
+        println("\nEnter values:-")
+        print("a: "); a = parse(Int, readline())
+        print("b: "); b = parse(Int, readline())
+        print("c: "); c = parse(Int, readline())
+        
+        QuadEqua(a, b, c)
+    catch
+        @warn "Enter a valid value"
+        continue
+    end
+    
+    print("\nDo you want to perform another operation (Y/N): ")
+    operation = readline()
+
+    if operation != "y" && uppercase(operation) != "Y"
+        break
     end
 end
